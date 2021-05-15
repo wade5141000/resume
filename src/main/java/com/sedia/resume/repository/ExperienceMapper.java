4b0ead4f443214c1a47a7b70f4324e83f2a8ac26
CCT\The_A <theairofcloudflying@gmail.com>
2021-05-15 23:12:43 +0800

    batis裡select id要參照method

--- src/main/java/com/sedia/resume/repository/ExperienceMapper.java
+++ src/main/java/com/sedia/resume/repository/ExperienceMapper.java
@@ -7,16 +7,13 @@ import com.sedia.resume.entity.ExperienceEntity;
 
 public interface ExperienceMapper {
 
-    // 經歷資料表 查詢首筆符合資料
-    // 是以sn?uid?Str?做查詢
-    // 概念是存取資料時，一定要有sn,uid做存取，新增時則要實體去指派資料
-    Optional<ExperienceEntity> firstExperience(int sn, int uid);
+    Optional<ExperienceEntity> firstExperience(int sn);
 
     // 經歷資料表 查詢資料清單
     List<ExperienceEntity> listExperience(int uid);
 
     // 經歷資料表 新增資料
-    ExperienceEntity insertExperience(ExperienceEntity user);
+    void insertExperience(ExperienceEntity user);
 
     // 經歷資料表 修改資料
     ExperienceEntity updateExperience(ExperienceEntity user);
