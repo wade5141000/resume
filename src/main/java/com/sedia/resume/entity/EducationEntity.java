9151914dd1668c014d596a9fd8e3c9ea50bf6606
Wade <wade5141000@outlook.com>
2021-05-16 19:41:54 +0800

    add constructor

--- src/main/java/com/sedia/resume/entity/EducationEntity.java
+++ src/main/java/com/sedia/resume/entity/EducationEntity.java
@@ -1,13 +1,14 @@
 package com.sedia.resume.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EducationEntity  {
 	
 	// Primary key
