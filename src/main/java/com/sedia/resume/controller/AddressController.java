94a9af1e8c20819f9d317875c5f0bbf036e0d4bf
wade5141000 <46486301+wade5141000@users.noreply.github.com>
2021-07-12 15:59:45 +0800

    remove auto value

--- src/main/java/com/sedia/resume/controller/AddressController.java
+++ src/main/java/com/sedia/resume/controller/AddressController.java
@@ -13,20 +13,18 @@ import java.util.List;
 @RequestMapping("/address")
 public class AddressController {
 
+    @GetMapping("/cities")
+    public List<String> getCities() {
+        // TODO 回傳台灣所有縣市名稱
+        // TODO cache 1hr
+        return null;
+    }
 
-  @GetMapping("/cities")
-  public List<String> getCities(){
-    // TODO 回傳台灣所有縣市名稱
-    // TODO cache 1hr
-    return null;
-  }
-
-  @GetMapping("/towns/{city}")
-  public List<String> getTowns(@PathVariable String city){
-    // TODO 給一個縣市名稱，回傳該縣市的鄉鎮市區資訊
-    // TODO cache 1hr
-    return null;
-  }
-
+    @GetMapping("/towns/{city}")
+    public List<String> getTowns(@PathVariable String city) {
+        // TODO 給一個縣市名稱，回傳該縣市的鄉鎮市區資訊
+        // TODO cache 1hr
+        return null;
+    }
 
 }
