7e18ecae6a0e7e3672d4bba0613d35f6011caedf
gitsky09 <59091092+gitsky09@users.noreply.github.com>
2021-07-21 23:07:13 +0800

    使用者圖片上傳

--- src/main/java/com/sedia/resume/controller/UserController.java
+++ src/main/java/com/sedia/resume/controller/UserController.java
@@ -2,19 +2,20 @@ package com.sedia.resume.controller;
 
 import com.sedia.resume.entity.UserEntity;
 import com.sedia.resume.service.UserService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ClassPathResource;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
+
 import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/user") // 在user根路徑
 public class UserController {
 
     final UserService service;
@@ -38,10 +39,13 @@ public class UserController {
     /**
      * TODO 1. 將圖片存到 resources/user/{account}/profile/{檔案名稱} (以後要換到 AWS S3 上) 2. 檔案名稱要替換，上傳新的圖片，舊的要刪掉 3. 儲存路徑寫入 user
      * imgPath 欄位 4. 回傳成功 or 失敗
+     * 
+     * @throws IOException
      */
     @PostMapping(value = "/image/upload", consumes = "multipart/form-data")
-    public boolean uploadImage(@RequestParam("image") MultipartFile image) {
-        return false;
+    public boolean uploadImage(@RequestParam("image") MultipartFile image) throws IOException {
+
+        return service.uptoS3(image);
     }
 
     /**
