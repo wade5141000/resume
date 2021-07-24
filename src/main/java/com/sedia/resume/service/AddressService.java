d2281589e36c7c6711b09c404e77ed7870985f15
Wade <wade5141000@outlook.com>
2021-07-24 22:34:47 +0800

    fix 地址 and 快取 debug log

--- src/main/java/com/sedia/resume/service/AddressService.java
+++ src/main/java/com/sedia/resume/service/AddressService.java
@@ -1,48 +1,35 @@
 
 package com.sedia.resume.service;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import com.sedia.resume.repository.AddressMapper;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.cache.annotation.CacheConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
-@CacheConfig(cacheNames = "caffeineCacheManager")
 public class AddressService {
 
     final AddressMapper addressMapper;
 
-    Cache<String, Object> cache = Caffeine.newBuilder().expireAfterWrite(12, TimeUnit.HOURS).initialCapacity(100)
-            .maximumSize(1000).build();
-
-    @Cacheable(value = "cache")
+    @Cacheable("address")
     public List<String> getCityList() {
-        System.out.print("取得所有城市資料");
-        return addressMapper.getAllCities();
+        log.debug("取得所有城市資料");
+        return addressMapper.getAllCities().stream().distinct().collect(Collectors.toList());
     }
 
-    @Cacheable(value = "cache", key = "#city")
+    @Cacheable(value = "address", key = "#city")
     public List<String> getTownList(String city) {
-        System.out.print("取得所有鄉鎮資料");
+        log.debug("取得 {} 的所有鄉鎮資料", city);
         return addressMapper.getAllTowns(city);
     }
-
-    // public String selectCity(int id) {
-    // return addressMapper.selectCityById(id);
-    // }
-    //
-    // public String selectTown(int id) {
-    // return addressMapper.selectTownById(id);
-    // }
 
 }
