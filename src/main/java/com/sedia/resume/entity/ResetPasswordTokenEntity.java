81d9cee30beab43d8a5a224ff5d8d65d2397847e
CCT\The_A <theairofcloudflying@gmail.com>
2021-07-31 15:12:57 +0800

    Revert "共用部分"
    This reverts commit 8c459509a7acf65d9f9e9ed09448143c176bf762.

--- src/main/java/com/sedia/resume/entity/ResetPasswordTokenEntity.java
+++ src/main/java/com/sedia/resume/entity/ResetPasswordTokenEntity.java
@@ -18,14 +18,5 @@ public class ResetPasswordTokenEntity extends GenericEntity {
     private LocalDateTime expiryDate;
 
     private boolean isUsed;
-    
-    // 建立人
-    private String crUser;
-    // 建立時間
-    private LocalDateTime crDatetime;
-    // 更新人
-    private String upUser;
-    // 更新時間
-    private LocalDateTime upDatetime;
 
 }
