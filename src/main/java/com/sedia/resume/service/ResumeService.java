2c147d770f9878adf2fb618ac53666cf9d17add5
Vicky Kuo <vicky.kuo.contact@gmail.com>
2021-09-05 00:02:34 +0800

    no message

--- src/main/java/com/sedia/resume/service/ResumeService.java
+++ src/main/java/com/sedia/resume/service/ResumeService.java
@@ -100,4 +100,57 @@ public class ResumeService {
             return false;
         }
     }
+    
+    // 使用者挑選resumeId根據自己需要的eduId加入
+    public boolean updateResumeEducation(int id, List<Integer> educationId) {
+        // 資料庫rel_resume_edu table的RID跟EDU_ID都是PrimeKey
+        resumeMapper.deleteResumeEducation(id);
+
+        for (int i = 0; i < educationId.size(); i++) {
+            resumeMapper.insertResumeEducation(id, educationId.get(i));
+        }
+
+        return true;
+    }
+
+    public boolean updateResumeExperience(int id, List<Integer> experienceId) {
+        resumeMapper.deleteResumeExperience(id);
+
+        for (int i = 0; i < experienceId.size(); i++) {
+            resumeMapper.insertResumeExperience(id, experienceId.get(i));
+        }
+
+        return true;
+    }
+
+    public boolean updateResumeLanguage(int id, List<Integer> languageId) {
+        resumeMapper.deleteResumeLanguage(id);
+
+        for (int i = 0; i < languageId.size(); i++) {
+            resumeMapper.insertResumeLanguage(id, languageId.get(i));
+        }
+
+        return true;
+    }
+
+    public boolean updateResumeLicense(int id, List<Integer> licenseId) {
+        resumeMapper.deleteResumeLicense(id);
+
+        for (int i = 0; i < licenseId.size(); i++) {
+            resumeMapper.insertResumeLicense(id, licenseId.get(i));
+        }
+
+        return true;
+    }
+
+    public boolean updateResumeSkill(int id, List<Integer> skillId) {
+        resumeMapper.deleteResumeSkill(id);
+
+        for (int i = 0; i < skillId.size(); i++) {
+            resumeMapper.insertResumeSkill(id, skillId.get(i));
+        }
+
+        return true;
+    }
+
 }
